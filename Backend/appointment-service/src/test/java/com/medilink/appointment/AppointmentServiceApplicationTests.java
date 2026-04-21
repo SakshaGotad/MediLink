350b59a42f2812906329a524c4d859b835353b32
SakshaGotad <sakshagotad@gmail.com>
2026-04-21 15:54:27 +0530

    Initial clean monorepo setup

---
+++ Backend/appointment-service/src/test/java/com/medilink/appointment/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilink.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
