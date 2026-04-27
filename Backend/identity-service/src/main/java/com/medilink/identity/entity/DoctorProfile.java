3d4aea55aa556a531a66384b103032d6c83be2b7
SakshaGotad <sakshagotad@gmail.com>
2026-04-27 16:29:49 +0530

    feat: add DoctorProfile and PatientProfile entities and repositories for identity management

---
+++ Backend/identity-service/src/main/java/com/medilink/identity/entity/DoctorProfile.java
@@ -0,0 +1,43 @@
+package com.medilink.identity.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DoctorProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 🔥 REQUIRED
+    @NotBlank()
+    private String specialization;
+    
+    private int experience;
+
+    private double fee;
+    
+    @NotBlank()
+    private String qualification;
+
+    @NotBlank()
+    private String licenseNumber;
+
+    // ⚡ OPTIONAL
+    private String clinicName;
+    private String clinicAddress;
+    private String bio;
+    private String availableDays;
+    private String profileImageUrl;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
